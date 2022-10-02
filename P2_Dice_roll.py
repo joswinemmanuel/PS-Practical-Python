65e40f2aeec0b75369ca62d3d0a441c794ef0c14
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-10-02 12:52:56 +0530

    Add files via upload

--- P2_Dice_roll.py
+++ P2_Dice_roll.py
@@ -4,6 +4,13 @@ roll = random.randint(1, 6)
 guess = int(input("Guess the dice roll : "))
 
 if guess == roll:
-    print(f"You gussed right!! The roll was {roll}")
+    print(f"You guessed right!! The roll was {roll}")
 else:
     print(f"Sorry :(  The roll was {roll}")
+
+
+# Guess the dice roll : 6
+# Sorry :(  The roll was 5
+
+# Guess the dice roll : 3
+# You guessed right!! The roll was 3
